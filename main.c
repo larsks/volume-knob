cc6008df85509b4db4c122dbd82ce7d5f68180cc
Lars Kellogg-Stedman <lars@oddbit.com>
2026-05-31 23:20:16 -0400

    chore: Reformat files

--- main.c
+++ main.c
@@ -10,10 +10,7 @@
 // Index: (prev_AB << 2) | curr_AB
 // Values: 0 = invalid/no change, 1 = CW, -1 = CCW
 static const int8_t encoder_table[16] = {
-     0, -1,  1,  0,
-     1,  0,  0, -1,
-    -1,  0,  0,  1,
-     0,  1, -1,  0,
+    0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0,
 };
 
 static uint8_t encoder_prev_state;
@@ -21,71 +18,71 @@ static int32_t encoder_accum;
 static bool key_is_pressed;
 
 static uint8_t read_encoder_state(void) {
-    uint8_t a = gpio_get(ENCODER_PIN_A) ? 1 : 0;
-    uint8_t b = gpio_get(ENCODER_PIN_B) ? 1 : 0;
-    return (a << 1) | b;
+  uint8_t a = gpio_get(ENCODER_PIN_A) ? 1 : 0;
+  uint8_t b = gpio_get(ENCODER_PIN_B) ? 1 : 0;
+  return (a << 1) | b;
 }
 
 static void encoder_init(void) {
-    gpio_init(ENCODER_PIN_A);
-    gpio_set_dir(ENCODER_PIN_A, GPIO_IN);
-    gpio_pull_up(ENCODER_PIN_A);
+  gpio_init(ENCODER_PIN_A);
+  gpio_set_dir(ENCODER_PIN_A, GPIO_IN);
+  gpio_pull_up(ENCODER_PIN_A);
 
-    gpio_init(ENCODER_PIN_B);
-    gpio_set_dir(ENCODER_PIN_B, GPIO_IN);
-    gpio_pull_up(ENCODER_PIN_B);
+  gpio_init(ENCODER_PIN_B);
+  gpio_set_dir(ENCODER_PIN_B, GPIO_IN);
+  gpio_pull_up(ENCODER_PIN_B);
 
-    encoder_prev_state = read_encoder_state();
-    encoder_accum = 0;
-    key_is_pressed = false;
+  encoder_prev_state = read_encoder_state();
+  encoder_accum = 0;
+  key_is_pressed = false;
 }
 
 static bool send_consumer_key(uint16_t usage) {
-    if (!tud_hid_ready())
-        return false;
-    tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &usage, sizeof(usage));
-    key_is_pressed = (usage != 0);
-    return true;
+  if (!tud_hid_ready())
+    return false;
+  tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &usage, sizeof(usage));
+  key_is_pressed = (usage != 0);
+  return true;
 }
 
 static void encoder_task(void) {
-    uint8_t curr = read_encoder_state();
-    uint8_t index = (encoder_prev_state << 2) | curr;
-    encoder_prev_state = curr;
+  uint8_t curr = read_encoder_state();
+  uint8_t index = (encoder_prev_state << 2) | curr;
+  encoder_prev_state = curr;
 
-    int8_t delta = encoder_table[index];
-    if (delta == 0)
-        return;
+  int8_t delta = encoder_table[index];
+  if (delta == 0)
+    return;
 
-    encoder_accum += delta;
+  encoder_accum += delta;
 
-    int32_t threshold = ENCODER_DIVIDER * ENCODER_STEPS_PER_DETENT;
-    if (encoder_accum >= threshold) {
-        if (send_consumer_key(KEY_CW))
-            encoder_accum = 0;
-    } else if (encoder_accum <= -threshold) {
-        if (send_consumer_key(KEY_CCW))
-            encoder_accum = 0;
-    }
+  int32_t threshold = ENCODER_DIVIDER * ENCODER_STEPS_PER_DETENT;
+  if (encoder_accum >= threshold) {
+    if (send_consumer_key(KEY_CW))
+      encoder_accum = 0;
+  } else if (encoder_accum <= -threshold) {
+    if (send_consumer_key(KEY_CCW))
+      encoder_accum = 0;
+  }
 }
 
 static void release_task(void) {
-    if (key_is_pressed && tud_hid_ready()) {
-        send_consumer_key(0);
-    }
+  if (key_is_pressed && tud_hid_ready()) {
+    send_consumer_key(0);
+  }
 }
 
 int main(void) {
-    board_init();
-    tusb_init();
-    stdio_init_all();
-    encoder_init();
+  board_init();
+  tusb_init();
+  stdio_init_all();
+  encoder_init();
 
-    while (1) {
-        tud_task();
-        encoder_task();
-        release_task();
-    }
+  while (1) {
+    tud_task();
+    encoder_task();
+    release_task();
+  }
 }
 
 void tud_mount_cb(void) {}
@@ -94,22 +91,22 @@ void tud_suspend_cb(bool remote_wakeup_en) { (void)remote_wakeup_en; }
 void tud_resume_cb(void) {}
 
 uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
-                                hid_report_type_t report_type,
-                                uint8_t *buffer, uint16_t reqlen) {
-    (void)instance;
-    (void)report_id;
-    (void)report_type;
-    (void)buffer;
-    (void)reqlen;
-    return 0;
+                               hid_report_type_t report_type, uint8_t *buffer,
+                               uint16_t reqlen) {
+  (void)instance;
+  (void)report_id;
+  (void)report_type;
+  (void)buffer;
+  (void)reqlen;
+  return 0;
 }
 
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
-                            hid_report_type_t report_type,
-                            uint8_t const *buffer, uint16_t bufsize) {
-    (void)instance;
-    (void)report_id;
-    (void)report_type;
-    (void)buffer;
-    (void)bufsize;
+                           hid_report_type_t report_type, uint8_t const *buffer,
+                           uint16_t bufsize) {
+  (void)instance;
+  (void)report_id;
+  (void)report_type;
+  (void)buffer;
+  (void)bufsize;
 }
--- tusb_config.h
+++ tusb_config.h
@@ -3,10 +3,10 @@
 
 #define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
 
-#define CFG_TUD_HID    1
-#define CFG_TUD_CDC    1
-#define CFG_TUD_MSC    0
-#define CFG_TUD_MIDI   0
+#define CFG_TUD_HID 1
+#define CFG_TUD_CDC 1
+#define CFG_TUD_MSC 0
+#define CFG_TUD_MIDI 0
 #define CFG_TUD_VENDOR 0
 
 #define CFG_TUD_HID_EP_BUFSIZE 16
